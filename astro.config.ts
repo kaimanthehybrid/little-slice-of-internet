37996d11576ac053eb0c905f897e5eee53470eec
Kaiman <lormenvv@gmail.com>
2026-01-04 19:27:59 -0600

    added friends and art, adjusted theme

--- astro.config.ts
+++ astro.config.ts
@@ -1,65 +1,61 @@
-import mdx from '@astrojs/mdx';
-import node from '@astrojs/node';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import expressiveCode from 'astro-expressive-code';
-import { loadEnv } from 'vite';
-import spectre, { type GiscusMapping } from './package/src';
-import { spectreDark } from './src/ec-theme';
+import mdx from "@astrojs/mdx";
+import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+import { loadEnv } from "vite";
+import spectre, { type GiscusMapping } from "./package/src";
+import { spectreDark } from "./src/ec-theme";
 
 const {
-	GISCUS_REPO,
-	GISCUS_REPO_ID,
-	GISCUS_CATEGORY,
-	GISCUS_CATEGORY_ID,
-	GISCUS_MAPPING,
-	GISCUS_STRICT,
-	GISCUS_REACTIONS_ENABLED,
-	GISCUS_EMIT_METADATA,
-	GISCUS_LANG,
-} = loadEnv(process.env.NODE_ENV!, process.cwd(), '');
+  GISCUS_REPO,
+  GISCUS_REPO_ID,
+  GISCUS_CATEGORY,
+  GISCUS_CATEGORY_ID,
+  GISCUS_MAPPING,
+  GISCUS_STRICT,
+  GISCUS_REACTIONS_ENABLED,
+  GISCUS_EMIT_METADATA,
+  GISCUS_LANG,
+} = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
 
 // https://astro.build/config
 const config = defineConfig({
-	site: 'https://spectre.lou.gg',
-	output: 'static',
-	integrations: [
-		expressiveCode({
-			themes: [spectreDark],
-		}),
-		mdx(),
-		sitemap(),
-		spectre({
-			name: 'Spectre',
-			openGraph: {
-				home: {
-					title: 'Spectre',
-					description: 'A minimalistic theme for Astro.',
-				},
-				blog: {
-					title: 'Blog',
-					description: 'News and guides for Spectre.',
-				},
-				projects: {
-					title: 'Projects',
-				},
-			},
-			giscus: {
-				repository: GISCUS_REPO,
-				repositoryId: GISCUS_REPO_ID,
-				category: GISCUS_CATEGORY,
-				categoryId: GISCUS_CATEGORY_ID,
-				mapping: GISCUS_MAPPING as GiscusMapping,
-				strict: GISCUS_STRICT === 'true',
-				reactionsEnabled: GISCUS_REACTIONS_ENABLED === 'true',
-				emitMetadata: GISCUS_EMIT_METADATA === 'true',
-				lang: GISCUS_LANG,
-			},
-		}),
-	],
-	adapter: node({
-		mode: 'standalone',
-	}),
+  site: "https://spectre.lou.gg",
+  output: "static",
+  integrations: [
+    expressiveCode({
+      themes: [spectreDark],
+    }),
+    mdx(),
+    sitemap(),
+    spectre({
+      name: "Kaiman the Hybrid!",
+      openGraph: {
+        home: {
+          title: "crocowolf cove",
+          description:
+            "The superior species for anthropomorphic animals everywhere.",
+        },
+        blog: {
+          title: "Blog",
+          description: "Posts and ramblings from the animal themselves.",
+        },
+        projects: {
+          title: "Projects",
+        },
+        friends: {
+          title: "Friends",
+        },
+        art: {
+          title: "Art",
+        },
+      },
+    }),
+  ],
+  adapter: node({
+    mode: "standalone",
+  }),
 });
 
 export default config;
